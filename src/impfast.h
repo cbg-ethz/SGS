bd9b4ba38d60ba2d7dc112d83ec2064069da53db
PhysFritz <fritzbayer@ymail.com>
2022-02-17 10:53:20 +0100

    removed R man scr

--- src/impfast.h
+++
@@ -1,9 +0,0 @@
-#include <R.h>
-#include <Rinternals.h>
-#include <math.h>
-
-/* Compute the Heterogeneous Euclidean Overlap Metric between vector vec and each row of matrix 
-   mat
-*/
-SEXP SubGroupSeparation_heom_dist( SEXP sexp_vec, SEXP sexp_mat, SEXP sexp_num_var, SEXP sexp_num_var_range );
-
